8562a95e9864f39abc3ee263fbea9008ba046807
Zakharov Anton <zakharov.toxa@gmail.com>
2020-08-11 20:24:27 +0200

    Cardboard
    voted card style

--- app/src/components/Room/Cardboard/Cardboard.tsx
+++ app/src/components/Room/Cardboard/Cardboard.tsx
@@ -2,6 +2,8 @@ import React, { useCallback, useState } from 'react';
 import { useIntl } from 'react-intl';
 import { Card, Col, Row, Typography } from 'antd';
 
+import { CARDS_VALUES } from '../../../constants';
+
 import messages from './messages';
 
 import './Cardboard.css';
@@ -16,16 +18,16 @@ const { Title } = Typography;
 const Cardboard: React.FC<CardboardProps> = ({ storyTitle, handleVote }) => {
   const intl = useIntl();
 
-  const [userVoted, setUserVoted] = useState(false);
+  const [userVote, setUserVote] = useState<number | boolean>(false);
 
   const onCardClick = useCallback(
     (points) => {
-      if (!userVoted) {
+      if (!userVote) {
         handleVote(points);
-        setUserVoted(true);
+        setUserVote(points);
       }
     },
-    [userVoted, handleVote, setUserVoted],
+    [userVote, handleVote, setUserVote],
   );
 
   return (
@@ -35,48 +37,17 @@ const Cardboard: React.FC<CardboardProps> = ({ storyTitle, handleVote }) => {
       </Title>
       <div className="Cardboard__Holder">
         <Row gutter={[16, 16]}>
-          <Col span={6}>
-            <Card onClick={() => onCardClick(0)} hoverable>
-              0
-            </Card>
-          </Col>
-          <Col span={6}>
-            <Card onClick={() => onCardClick(0.5)} hoverable>
-              ½
-            </Card>
-          </Col>
-          <Col span={6}>
-            <Card onClick={() => onCardClick(1)} hoverable>
-              1
-            </Card>
-          </Col>
-          <Col span={6}>
-            <Card onClick={() => onCardClick(2)} hoverable>
-              2
-            </Card>
-          </Col>
-        </Row>
-        <Row gutter={[16, 16]}>
-          <Col span={6}>
-            <Card onClick={() => onCardClick(3)} hoverable>
-              3
-            </Card>
-          </Col>
-          <Col span={6}>
-            <Card onClick={() => onCardClick(4)} hoverable>
-              5
-            </Card>
-          </Col>
-          <Col span={6}>
-            <Card onClick={() => onCardClick(5)} hoverable>
-              8
-            </Card>
-          </Col>
-          <Col span={6}>
-            <Card onClick={() => onCardClick(1000)} hoverable>
-              ?
-            </Card>
-          </Col>
+          {CARDS_VALUES.map((point) => (
+            <Col span={6} key={point}>
+              <Card
+                className={`Card${userVote === point ? ' active' : ''}`}
+                onClick={() => onCardClick(point)}
+                hoverable
+              >
+                {point === 0.5 ? '½' : point}
+              </Card>
+            </Col>
+          ))}
         </Row>
       </div>
     </div>
