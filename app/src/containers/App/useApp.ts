74993498260c7286ae058ef0cf2679333d2c5d50
Zakharov Anton <zakharov.toxa@gmail.com>
2020-09-16 15:13:39 +0200

    Docker, remove bootstrap

--- app/src/containers/App/useApp.ts
+++ app/src/containers/App/useApp.ts
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 import { LS_KEY } from '../../constants';
 
 const useApp = () => {
-  const socket = io.connect('http://localhost:3001');
+  const socket = io.connect('http://localhost:3011');
   const localUserId = localStorage.getItem(LS_KEY) || '';
 
   return { localUserId, socket };
